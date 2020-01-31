f3f57712daf16521c0a32e769f021ef7f34a5765
Kye W. Shi <kwshi@hmc.edu>
2020-01-30 20:59:33 -0800

    remove old reason stuff, use npm because netlify doesn't officially support elm

--- old/docker.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm install
-
-export PATH=$(pwd)/node_modules/.bin:$PATH
-
-exec bash
--- old/src/main.js
+++
@@ -1,4 +0,0 @@
-const refocus = () => document.getElementById('currentInput').focus();
-setInterval(refocus, 100);
-
-require('./App.re').main(document.getElementById('terminal'));
